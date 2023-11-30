4fa00640ada374bde48e0aa2752bb85fecaed4b8
ahmet yilmaz <ahmet.79@hotmail.be>
2023-11-30 15:28:58 +0100

    Properties for the Category Class (Model)

---
+++ Bulky_MVC/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Bulky_MVC.Models
+{
+    public class Category
+    {
+        #region PROPERTIES
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        #endregion PROPERTIES
+    }
+}
