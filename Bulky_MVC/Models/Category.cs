f52c3fa5172aefce83679554327db19c2c8dc81b
ahmet yilmaz <ahmet.79@hotmail.be>
2023-11-30 15:34:09 +0100

    Simple Data annotation in Category class (Model)

--- Bulky_MVC/Models/Category.cs
+++ Bulky_MVC/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace Bulky_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky_MVC.Models
 {
     public class Category
     {
         #region PROPERTIES
+        //Data Annotation = [something],
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         #endregion PROPERTIES
