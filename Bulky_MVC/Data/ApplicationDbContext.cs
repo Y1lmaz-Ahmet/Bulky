ce2d522c8f5c19cb95788062dd6d35d389b4971c
ahmet yilmaz <ahmet.79@hotmail.be>
2023-11-30 16:30:14 +0100

    adding connectionstring to the project

---
+++ Bulky_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
