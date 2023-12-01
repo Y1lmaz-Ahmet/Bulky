2497bcef6dc0b69c54f1a1759808f316e72f687e
ahmet yilmaz <ahmet.79@hotmail.be>
2023-12-01 10:10:50 +0100

    SeedCategory Table with new Data using Migration

--- Bulky_MVC/Data/ApplicationDbContext.cs
+++ Bulky_MVC/Data/ApplicationDbContext.cs
@@ -15,5 +15,14 @@ namespace Bulky_MVC.Data
         #region Creating Tables
         public DbSet<Category> Categories { get; set; }
         #endregion Creating Tables
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
