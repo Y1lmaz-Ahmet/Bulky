a9fca521190146541ff3917415b4f8db17570285
ahmet yilmaz <ahmet.79@hotmail.be>
2023-12-01 13:06:48 +0100

    YAY! adding functionality for the Category Controller & comments to make code understandable

--- Bulky_MVC/Controllers/CategoryController.cs
+++ Bulky_MVC/Controllers/CategoryController.cs
@@ -6,21 +6,29 @@ namespace Bulky_MVC.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db; // accessing database 
         public CategoryController(ApplicationDbContext db)
         {
-            _db = db;
+            _db = db; // make instance of database & set property 
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
+            List<Category> objCategoryList = _db.Categories.ToList(); // make a list of categorie objects
+            return View(objCategoryList); // show the categorie objects in the Index View
         }
 
         //return the View for CreateCategory
         public IActionResult Create()
         {
             return View();
+        }
+        //create a new Category and add it to the database - [httpPost] for posting in MVC
+        [HttpPost]
+        public IActionResult Create(Category obj) 
+        { 
+            _db.Categories.Add(obj); // add a new Category Object to the Database
+            _db.SaveChanges(); // Go to the Database and Save the new Object
+            return RedirectToAction("Index"); // return to the List of Categories, refers to Index Action
         }
     }
 }
