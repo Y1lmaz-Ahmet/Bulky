f4ea5fb1e814cbba057cdb951e83f429c9507526
ahmet yilmaz <ahmet.79@hotmail.be>
2023-12-01 10:16:25 +0100

    default code to communicate with DB using EF

--- Bulky_MVC/Controllers/CategoryController.cs
+++ Bulky_MVC/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky_MVC.Data;
+using Bulky_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky_MVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
