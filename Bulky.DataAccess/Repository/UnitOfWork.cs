7055a7e817b774dd79dfc4fc8894d5a0347ac050
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2023-12-05 14:14:42 +0100

    UnitOfWork!

---
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky_MVC.DataAcess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category{  get;private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
