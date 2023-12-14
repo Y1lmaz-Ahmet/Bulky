f87c64972d89b6598c7d2199923cd6628b9671dc
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2023-12-14 15:47:16 +0100

    creating the products table in database + seeding with objects.

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Bulky;
 using Bulky.Models;
+using BulkyBook.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bulky_MVC.DataAcess.Data
@@ -14,9 +15,11 @@ namespace Bulky_MVC.DataAcess.Data
         #endregion Default Code to Configure DbContect Class
 
         #region Creating Tables
-        public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; } // makes a DataTable with the name Categories
+        public DbSet< Product> Products { get; set; } // makes a DataTable with the name Products
         #endregion Creating Tables
 
+        // Create 3 objects after table is made. Seeding the table
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
@@ -24,6 +27,62 @@ namespace Bulky_MVC.DataAcess.Data
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
+
+            modelBuilder.Entity<Product>().HasData(
+            new Product 
+            { 
+                Id = 1,
+                Title = "Book 1",
+                Description = "Description 1",
+                ISBN = "ISBN1",
+                Author = "Author 1",
+                ListPrice = 19.99,
+                Price = "14.99",
+                Price50 = 12.99,
+                Price100 = 10.99
+            },
+            new Product { 
+                Id = 2,
+                Title = "Book 2",
+                Description = "Description 2",
+                ISBN = "ISBN2",
+                Author = "Author 2",
+                ListPrice = 24.99,
+                Price = "19.99",
+                Price50 = 17.99,
+                Price100 = 15.99
+            },
+            new Product { Id = 3,
+                Title = "Book 3",
+                Description = "Description 3",
+                ISBN = "ISBN3",
+                Author = "Author 3",
+                ListPrice = 29.99,
+                Price = "24.99",
+                Price50 = 22.99,
+                Price100 = 20.99
+            },
+            new Product { Id = 4,
+                Title = "Book 4",
+                Description = "Description 4",
+                ISBN = "ISBN4",
+                Author = "Author 4",
+                ListPrice = 34.99,
+                Price = "29.99",
+                Price50 = 27.99,
+                Price100 = 25.99
+            },
+            new Product { Id = 5,
+                Title = "Book 5",
+                Description = "Description 5",
+                ISBN = "ISBN5",
+                Author = "Author 5",
+                ListPrice = 39.99,
+                Price = "34.99",
+                Price50 = 32.99,
+                Price100 = 30.99 
+            }
+        );
         }
     }
 }
---
+++ Bulky.DataAccess/Migrations/20231214144545_createProductTable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class createProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Author 1", "Description 1", "ISBN1", 19.989999999999998, "14.99", 10.99, 12.99, "Book 1" },
+                    { 2, "Author 2", "Description 2", "ISBN2", 24.989999999999998, "19.99", 15.99, 17.989999999999998, "Book 2" },
+                    { 3, "Author 3", "Description 3", "ISBN3", 29.989999999999998, "24.99", 20.989999999999998, 22.989999999999998, "Book 3" },
+                    { 4, "Author 4", "Description 4", "ISBN4", 34.990000000000002, "29.99", 25.989999999999998, 27.989999999999998, "Book 4" },
+                    { 5, "Author 5", "Description 5", "ISBN5", 39.990000000000002, "34.99", 30.989999999999998, 32.990000000000002, "Book 5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
