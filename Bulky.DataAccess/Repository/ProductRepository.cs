531ded2bbd7b9cbdab49589e876a3ff08fe7fe3c
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2023-12-14 16:04:46 +0100

    repository for product

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAccess.Repository;
+using Bulky_MVC.DataAcess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+
+    }
+}
